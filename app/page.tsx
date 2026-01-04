95d7452cc766f4ed0caf2261bd1d1924e1115c4b
Nicholas <et738102@gmail.com>
2026-01-04 21:07:18 +0000

    (testing) Deploy vercel app

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@mantine/core';
 export default function Home() {
   return (
     <div>
-      <Button>Hello</Button>
+      <Button>Hello my friend</Button>
     </div>
   )
 }
